e332e93248912bea06d5a1c31d70298ea39fa589
Yuqi_Lu <lu_yuqi@outlook.com>
2021-03-30 21:17:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UE4_Lecture
+
+Developed with Unreal Engine 4
---
+++ Source/UE4_Lecture/UE4_LectureGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_LectureGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_LECTURE_API AUE4_LectureGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
